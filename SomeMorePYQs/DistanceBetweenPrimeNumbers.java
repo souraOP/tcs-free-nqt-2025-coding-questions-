ee27b7ae9a3a1fdb1019fb362f9bd5ffe107a832
souraOP <sourasishmondal95@gmail.com>
2025-03-14 20:56:33 +0530

    changed logic

--- SomeMorePYQs/DistanceBetweenPrimeNumbers.java
+++ SomeMorePYQs/DistanceBetweenPrimeNumbers.java
@@ -45,27 +45,30 @@ public class DistanceBetweenPrimeNumbers {
     }
 
     static int getMaxDistance(int[] arr){
-        int firstPrime = -1, lastPrime= -1;
-        for(int i = 0; i < arr.length; i++){
-            if(isPrime(arr[i])){
-                if(firstPrime == -1){
-                    firstPrime = i;
-                }
-                lastPrime = i;
+        int firstPrime = 0, lastPrime = arr.length - 1;
+        while(firstPrime < lastPrime){
+            if(isPrime(arr[firstPrime])){
+                break;
             }
-            // if prime number is not found
-            if(firstPrime == -1 || firstPrime == lastPrime){
-                return 0;
-            }
+            firstPrime++;
         }
-        return Math.abs(lastPrime - firstPrime);
+
+        // get the last prime
+        while(lastPrime >= firstPrime){
+            if(isPrime(arr[lastPrime])){
+                break;
+            }
+            lastPrime--;
+        }
+        return lastPrime - firstPrime;
+
     }
 
     static boolean isPrime(int i){
         if(i <= 1){
             return false;
         }
-        for(int j = 2; j < i/2; j++){
+        for(int j = 2; j <= Math.sqrt(i); j++){
             if(i % j == 0){
                 return false;
             }
